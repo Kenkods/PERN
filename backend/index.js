2b0debe449fd568d7de73b5850d5557f8ea7f0b3
John Kenneth Pantonial <jkennn.02@gmail.com>
2025-07-26 23:56:52 +0800

    Created frontend folder

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
 import express from "express";
+import cors from "cors";
 
 import addUserRoute from "./src/routes/addUserRoute.js";
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 app.use('/api',addUserRoute);
